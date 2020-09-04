285b3333a366d035ced26372f85d585f01b9ec3d
EitoTakishita <eito.takishita@gmail.com>
2020-09-04 10:32:02 +0900

    Add sqlite

--- lib/app/resorces/viewmodels/pepper_viewmodels.dart
+++ lib/app/resorces/viewmodels/pepper_viewmodels.dart
@@ -2,6 +2,8 @@ import 'package:flutter_pepper/app/repository/pepper_repository.dart';
 import 'package:flutter_pepper/app/resorces/api/pepper_api.dart';
 import 'package:flutter_pepper/app/resorces/models/model.dart';
 import 'package:flutter_pepper/app/resorces/viewmodels/base_viewmodels.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class PepperViewModel extends BaseViewModel {
   final PepperRepository repo = PepperRepository(PepperApiProvider());
@@ -14,6 +16,9 @@ class PepperViewModel extends BaseViewModel {
     if (results != null) {
       super.showErrorDialogController.sink.add(false);
       _shops = results.results;
+      final database = openDatabase(
+        join(await getDatabasesPath(), 'memo_database.db'),
+      );
       notifyListeners();
     } else {
       errorMessage = '店舗情報が取得できませんでした';
